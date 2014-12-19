62375fd20c6f20a2ad8dfdd12b75cabecb2500e1
weberius <eberius@gmail.com>
2014-12-19 05:52:28 +0100

    schueler im stadtteil in koeln

--- odk/src/test/java/de/illilli/opengis/service/SchuelerNachStadtteilFacadeSystemTest.java
+++ odk/src/test/java/de/illilli/opengis/service/SchuelerNachStadtteilFacadeSystemTest.java
@@ -1,14 +1,22 @@
 package de.illilli.opengis.service;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 public class SchuelerNachStadtteilFacadeSystemTest {
 
+	private final static Logger logger = Logger
+			.getLogger(SchuelerNachStadtteilFacadeSystemTest.class);
+
 	@Test
-	public void test() {
+	public void test() throws URISyntaxException, IOException {
 		String stadt = "köln";
 		SchuelerNachStadtteilFacade facade = new SchuelerNachStadtteilFacade(
 				stadt);
+		logger.debug(facade.getString());
 	}
 
 }
