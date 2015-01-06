5d1a3ceb15156d955cde747f3ce10cfb615339fe
weberius <eberius@gmail.com>
2015-01-06 05:32:04 +0100

    Altersstruktur

--- odk/src/main/java/de/illilli/opengis/odk/bo/EinwohnerNachAltersgruppenBo.java
+++ odk/src/main/java/de/illilli/opengis/odk/bo/EinwohnerNachAltersgruppenBo.java
@@ -75,4 +75,54 @@ public class EinwohnerNachAltersgruppenBo {
 		this.label = label;
 	}
 
+	@Override
+	public String toString() {
+		return "EinwohnerNachAltersgruppenBo [value=" + value + ", color="
+				+ color + ", highlight=" + highlight + ", label=" + label + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((color == null) ? 0 : color.hashCode());
+		result = prime * result
+				+ ((highlight == null) ? 0 : highlight.hashCode());
+		result = prime * result + ((label == null) ? 0 : label.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(value);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EinwohnerNachAltersgruppenBo other = (EinwohnerNachAltersgruppenBo) obj;
+		if (color == null) {
+			if (other.color != null)
+				return false;
+		} else if (!color.equals(other.color))
+			return false;
+		if (highlight == null) {
+			if (other.highlight != null)
+				return false;
+		} else if (!highlight.equals(other.highlight))
+			return false;
+		if (label == null) {
+			if (other.label != null)
+				return false;
+		} else if (!label.equals(other.label))
+			return false;
+		if (Double.doubleToLongBits(value) != Double
+				.doubleToLongBits(other.value))
+			return false;
+		return true;
+	}
+
 }
